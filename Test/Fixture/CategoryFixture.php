53468681b5af2316c6bacd086cbe623d1851e3a2
paolobueno <paolohaji@gmail.com>
2013-01-29 15:15:41 -0200

    geracao de codigo para categorias

---
+++ Test/Fixture/CategoryFixture.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * CategoryFixture
+ *
+ */
+class CategoryFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'descricao' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'imagem' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'indexes' => array(
+			
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'descricao' => 'Lorem ipsum dolor sit amet',
+			'imagem' => 'Lorem ipsum dolor sit amet'
+		),
+	);
+
+}
